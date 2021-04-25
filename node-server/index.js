ed474278ec78d2e6183ca4f4ced614a0c0b19dad
Suleyman Taspinar <suleyman@pranageo.com>
2021-04-25 19:57:18 +0300

    app: Fix console log

--- node-server/index.js
+++ node-server/index.js
@@ -3,5 +3,6 @@ const app = require('./app.js');
 const PORT = 5000;
 const HOST = '0.0.0.0';
 
-app.listen(PORT, HOST);
-console.log(`Listening on ${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://${HOST}:${PORT}`);
+})
